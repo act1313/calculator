a09c55af84c3560e74e8d5fde36d15e88b917abf
act1313 <act3313@gmail.com>
2022-07-22 15:29:33 -0400

    made the screen responsive. finish later

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let topDisplay = document.querySelector('.lastNumber');
+let bottomDisplay = document.querySelector('.current');
+let numbers = document.querySelectorAll('.number');
+let clear = document.querySelector('.clear');
+let operators = document.querySelectorAll('.operator');
+let currentNumber = '';
+let currentOperation = '';
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +21,55 @@ function multiply(a, b) {
 function divide(a, b) { 
     return a / b;
 }
+
+function operator(a, b, sign) {
+    if (sign == '*') {
+        multiply(a, b);
+    } else if (sign == '+') {
+        add(a, b);
+    } else if (sign == '-') {
+        subtract(a, b);
+    } else if (sign == '/') {
+        divide(a, b);
+    } else {
+        throw new Error('Invalid operator: ' + sign);
+    }
+}
+
+function addBottomDisplay(num) {
+    bottomDisplay.innerHTML = num;
+}
+
+function addTopDisplay(operator) {
+    if (topDisplay.innerHTML != "") {
+        topDisplay.innerHTML += " " + currentNumber + " " + operator;
+    } else {
+        topDisplay.innerHTML += currentNumber + " " + operator;
+    }
+}
+
+function clearBottomDisplay() {
+    bottomDisplay.innerHTML = "";
+}
+
+function clearTopDisplay() {
+    topDisplay.innerHTML = " ";
+}
+
+clear.addEventListener('click', clearBottomDisplay());
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        currentNumber += number.innerHTML;
+        addBottomDisplay(currentNumber);
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        currentOperation = operator.innerHTML;
+        addTopDisplay(currentOperation);
+        clearBottomDisplay();
+        currentNumber = '';
+    });
+});
