56ec0913bd519bd405136829551ad9a2edeb79b5
act1313 <act3313@gmail.com>
2022-07-25 16:02:57 -0400

    finished all the required functionality

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let topDisplay = document.querySelector('.lastNumber');
 let bottomDisplay = document.querySelector('.current');
 let numbers = document.querySelectorAll('.number');
 let clear = document.querySelector('.clear');
 let operators = document.querySelectorAll('.operator');
-let currentNumber = '';
+let equals = document.querySelector('.equals');
+let deleteKey = document.querySelector('.delete');
+let num1 = '';
+let num2 = '';
+let answer = '';
 let currentOperation = '';
 
 function add(a, b) {
@@ -22,15 +25,15 @@ function divide(a, b) {
     return a / b;
 }
 
-function operator(a, b, sign) {
-    if (sign == '*') {
-        multiply(a, b);
+function theCalculatorJuice(a, b, sign) {
+    if (sign == 'x') {
+        return multiply(a, b);
     } else if (sign == '+') {
-        add(a, b);
+        return add(a, b);
     } else if (sign == '-') {
-        subtract(a, b);
-    } else if (sign == '/') {
-        divide(a, b);
+        return subtract(a, b);
+    } else if (sign == '÷') {
+        return divide(a, b);
     } else {
         throw new Error('Invalid operator: ' + sign);
     }
@@ -40,36 +43,63 @@ function addBottomDisplay(num) {
     bottomDisplay.innerHTML = num;
 }
 
-function addTopDisplay(operator) {
-    if (topDisplay.innerHTML != "") {
-        topDisplay.innerHTML += " " + currentNumber + " " + operator;
-    } else {
-        topDisplay.innerHTML += currentNumber + " " + operator;
-    }
-}
-
 function clearBottomDisplay() {
     bottomDisplay.innerHTML = "";
 }
 
-function clearTopDisplay() {
-    topDisplay.innerHTML = " ";
-}
+clear.addEventListener('click', () => {
+    bottomDisplay.innerHTML = "";
+    num1 = '';
+    num2 = '';
+    currentOperation = '';
+});
 
-clear.addEventListener('click', clearBottomDisplay());
+deleteKey.addEventListener('click', () => {
+    let bottom = bottomDisplay.innerHTML;
+    let editBottom = bottom.slice(0, -1);
+
+    bottomDisplay.innerHTML = editBottom;
+});
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        currentNumber += number.innerHTML;
-        addBottomDisplay(currentNumber);
+        if (num1 != '' && currentOperation != '') {
+            num2 += number.innerHTML;
+            addBottomDisplay(num2);
+        } else {
+            num1 += number.innerHTML;
+            addBottomDisplay(num1);
+        } 
     });
 });
 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        currentOperation = operator.innerHTML;
-        addTopDisplay(currentOperation);
-        clearBottomDisplay();
-        currentNumber = '';
+        if (num2 == '') {
+            currentOperation = operator.innerHTML;
+        } else {
+            let a = parseInt(num1);
+            let b = parseInt(num2);
+            answer = theCalculatorJuice(a, b, currentOperation);
+
+            num1 = answer;
+            num2 = '';
+
+            bottomDisplay.innerHTML = answer;
+            currentOperation = operator.innerHTML;
+        }
     });
+});
+
+equals.addEventListener('click', () => {
+    if (num2 != '') {
+        let a = parseInt(num1);
+        let b = parseInt(num2);
+        answer = theCalculatorJuice(a, b, currentOperation);
+
+        num1 = answer;
+        num2 = '';
+
+        bottomDisplay.innerHTML = num1;
+    }
 });
